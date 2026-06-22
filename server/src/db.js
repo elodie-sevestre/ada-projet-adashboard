3b99e653843bebedba280df3fea7e8567096f15a
Elodie S <elodie.sevestre86@gmail.com>
2026-06-22 07:33:32 +0200

    fix: correctif name db to database in server

--- server/src/db.js
+++ server/src/db.js
@@ -15,7 +15,7 @@ dotenv.config();
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST || "localhost",
-  db: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT || 5432,
 });
